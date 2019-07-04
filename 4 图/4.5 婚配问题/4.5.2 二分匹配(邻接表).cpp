3c2de56db7a601fe074fdf0c011f76656ec5e6a3
HuKeqing <keqing.hu@icloud.com>
2019-07-04 19:47:37 +0800

    婚配问题

---
+++ 4 图/4.5 婚配问题/4.5.2 二分匹配(邻接表).cpp
@@ -0,0 +1,58 @@
+/*
+ * 二分匹配(邻接表)
+ * 每组数据使用前使用 init 函数初始化，给 uN 赋值
+ * 加边使用函数 addedge(u, v)
+ * 点的编号 0~uN - 1点的编号 0~uN - 1
+ */
+
+const int MAXN = 5010;  //点数的最大值
+const int MAXM = 50010; //边数的最大值
+struct Edge
+{
+    int to, next;
+} edge[MAXM];
+int head[MAXN], tot;
+void init()
+{
+    tot = 0;
+    memset(head, -1, sizeof(head));
+}
+void addedge(int u, int v)
+{
+    edge[tot].to = v;
+    edge[tot].next = head[u];
+    head[u] = tot++;
+}
+int linker[MAXN];
+bool used[MAXN];
+int uN;                 // 点的个数
+bool dfs(int u)
+{
+    for (int i = head[u]; i != -1; i = edge[i].next)
+    {
+        int v = edge[i].to;
+        if (!used[v])
+        {
+            used[v] = true;
+            if (linker[v] == -1 || dfs(linker[v]))
+            {
+                linker[v] = u;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+int hungary()
+{
+    int res = 0;
+    memset(linker, -1, sizeof(linker));
+    // 点的编号 0~uN - 1
+    for (int u = 0; u < uN; u++)
+    {
+        memset(used, false, sizeof(used));
+        if (dfs(u))
+            res++;
+    }
+    return res;
+}
