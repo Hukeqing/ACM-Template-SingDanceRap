e24eb97b29e008960682f27589c26892d0885006
HuKeqing <keqing.hu@icloud.com>
2019-07-08 15:55:21 +0800

    Update

--- 4 图/4.7 网络流/4.7.1 最大流SAP算法.cpp
+++ 4 图/4.7 网络流/4.7.1 最大流SAP算法.cpp
@@ -3,15 +3,15 @@
  * 先把流量限制赋值到 maps 数组
  * 然后调用 SAP 函数求解
  */
-
-#define MAXN 222
+#define MAXN 500
 #define inf 100000000 + 1000
-int maps[MAXN][MAXN]; //存图
-int pre[MAXN];        //记录当前点的前驱
-int level[MAXN];      //记录距离标号
-int gap[MAXN];        //gap常数优化
 
-//入口参数vs源点，vt汇点
+int maps[MAXN][MAXN]; // 存图
+int pre[MAXN];        // 记录当前点的前驱
+int level[MAXN];      // 记录距离标号
+int gap[MAXN];        // gap常数优化
+
+// 入口参数vs源点，vt汇点
 int SAP(int vs, int vt)
 {
     memset(pre, -1, sizeof(pre));
@@ -22,7 +22,7 @@ int SAP(int vs, int vt)
 
     while (level[vs] < vt)
     {
-        //寻找可行弧
+        // 寻找可行弧
         for (v = 1; v <= vt; v++)
         {
             if (maps[u][v] > 0 && level[u] == level[v] + 1)
@@ -38,7 +38,7 @@ int SAP(int vs, int vt)
             {
                 int neck = 0;
                 aug = inf;
-                //寻找当前找到的一条路径上的最大流 , (瓶颈边)
+                // 寻找当前找到的一条路径上的最大流 , (瓶颈边)
                 for (int i = v; i != vs; i = pre[i])
                 {
                     if (aug > maps[pre[i]][i])
@@ -48,21 +48,22 @@ int SAP(int vs, int vt)
                     }
                 }
                 maxflow += aug;
-                //更新残留网络
+                // 更新残留网络
                 for (int i = v; i != vs; i = pre[i])
                 {
                     maps[pre[i]][i] -= aug;
                     maps[i][pre[i]] += aug;
                 }
-                u = vs; //从源点开始继续搜
-                        //		u=neck; 	  // Dnic 多路增广优化，下次增广时，从瓶颈边(后面)开始
+                u = vs; // 从源点开始继续搜
+                        // u=neck;
+                        // Dnic 多路增广优化，下次增广时，从瓶颈边(后面)开始
             }
         }
         else
         {
-            //找不到可行弧
+            // 找不到可行弧
             int minlevel = vt;
-            //寻找与当前点相连接的点中最小的距离标号
+            // 寻找与当前点相连接的点中最小的距离标号
             for (v = 1; v <= vt; v++)
             {
                 if (maps[u][v] > 0 && minlevel > level[v])
@@ -70,9 +71,9 @@ int SAP(int vs, int vt)
                     minlevel = level[v];
                 }
             }
-            gap[level[u]]--; //(更新gap数组）当前标号的数目减1；
+            gap[level[u]]--; // (更新gap数组）当前标号的数目减1；
             if (gap[level[u]] == 0)
-                break; //出现断层
+                break; // 出现断层
             level[u] = minlevel + 1;
             gap[level[u]]++;
             u = pre[u];
