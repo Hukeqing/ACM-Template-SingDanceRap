b8da279c01aebd15723d54cd922ba41ce9f11731
zhangyf <1691903105@qq.com>
2019-07-21 13:53:26 +0800

    update 关于素数判断的方法，快速确定单个数是否为素数

---
+++ 2 数学/2.1 代数/2.1.1 素数/2.1.5 判断素数.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+bool judge(int x){
+    float n_sqrt;
+    if(x == 2 || x == 3){
+        return true;
+    }
+    if(x % 6 != 1 && x % 6 != 1){
+        return false;
+    }
+    n_sqrt = floor(sqrt(float(x)));
+    for(int i = 5; i <= n_sqrt; i += 6){
+        if(x % i == 0 || x % (i + 2) == 0){
+            return false;
+        }
+    }
+    return true;
+}
