b86c412fd24e3aa0aca441fff934fef7302b7c0c
Mauve <keqing.hu@icloud.com>
2019-07-22 08:13:24 +0800

    Update

--- 2 数学/2.1 代数/2.1.7 快速幂.cpp
+++ 2 数学/2.1 代数/2.1.7 快速幂.cpp
@@ -1,8 +1,10 @@
 int pow_mod(int a, int n, int mod)
 {
-    if(n == 0) return 1;
-    int x = pow_mod(a, n/2, mod);
+    if (n == 0)
+        return 1;
+    int x = pow_mod(a, n / 2, mod);
     long long ans = (long long)x * x % mod;
-    if(n % 2 == 1) ans = ans *a % mod;
+    if (n % 2 == 1)
+        ans = ans * a % mod;
     return (int)ans;
 }
