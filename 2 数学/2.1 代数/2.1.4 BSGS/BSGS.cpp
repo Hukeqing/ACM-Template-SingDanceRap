0ff2576ad081b611f3a6d789ee04f92ba45231bf
zhangyufeng0123 <1691903105@qq.com>
2019-08-17 09:04:51 +0800

    zyf:change the BSGS

--- 2 数学/2.1 代数/2.1.4 BSGS/BSGS.cpp
+++ 2 数学/2.1 代数/2.1.4 BSGS/BSGS.cpp
@@ -3,13 +3,13 @@ void bsgs(ll y,ll z,ll p)//y^x=z(mod p)
     if(y==0 && z==0){puts("1");return ;}//几句特判 
     if(y==0 && z!=0){return;}//不存在
     
-    hash.clear();
+    mp.clear();
     ll m=ceil(sqrt(p));
-    ll tmp=z%p;hash[tmp]=0;//右边z*A^j，当j=0时为z 
+    ll tmp=z%p;mp[tmp]=0;//右边z*A^j，当j=0时为z 
     for(ll i=1;i<=m;i++)
     {
         tmp=tmp*y%p;
-        hash[tmp]=i;
+        mp[tmp]=i;
     }
     
     ll t=power(y,m,p); 
@@ -17,9 +17,9 @@ void bsgs(ll y,ll z,ll p)//y^x=z(mod p)
     for(ll i=1;i<=m;i++)
     {
         tmp=tmp*t%p;//i每加1，多乘y^(i*m)
-        if(hash[tmp])
+        if(mp[tmp])
         {
-            ll ans=i*m-hash[tmp];
+            ll ans=i*m-mp[tmp];
             printf("%lld\n",(ans%p+p)%p);
             return ;
         }
