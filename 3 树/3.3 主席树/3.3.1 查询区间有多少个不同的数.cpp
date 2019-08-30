4176eadf3cbd3a3279895258f8d0a8d1501087bb
EscapeReality846089495 <846089495@qq.com>
2019-08-30 09:49:11 +0800

    更新主席树板子

--- 3 树/3.3 主席树/3.3.1 查询区间有多少个不同的数.cpp
+++ 3 树/3.3 主席树/3.3.1 查询区间有多少个不同的数.cpp
@@ -1,3 +1,7 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
 /*
  * 给出一个序列，查询区间内有多少个不相同的数
  * 1、先把数组保存在 a 数组中
@@ -10,36 +14,31 @@ const int M = MAXN * 100;
 int n, q, tot;
 int a[MAXN];
 int T[MAXN], lson[M], rson[M], c[M];
-int __build(int l, int r)
-{
+
+int __build(int l, int r) {
     int root = tot++;
     c[root] = 0;
-    if (l != r)
-    {
+    if (l != r) {
         int mid = (l + r) >> 1;
         lson[root] = __build(l, mid);
         rson[root] = __build(mid + 1, r);
     }
     return root;
 }
-int update(int root, int pos, int val)
-{
+
+int update(int root, int pos, int val) {
     int newroot = tot++, tmp = newroot;
     c[newroot] = c[root] + val;
     int l = 1, r = n;
-    while (l < r)
-    {
+    while (l < r) {
         int mid = (l + r) >> 1;
-        if (pos <= mid)
-        {
+        if (pos <= mid) {
             lson[newroot] = tot++;
             rson[newroot] = rson[root];
             newroot = lson[newroot];
             root = lson[root];
             r = mid;
-        }
-        else
-        {
+        } else {
             rson[newroot] = tot++;
             lson[newroot] = lson[root];
             newroot = rson[newroot];
@@ -50,20 +49,16 @@ int update(int root, int pos, int val)
     }
     return tmp;
 }
-int query(int root, int pos)
-{
+
+int query(int root, int pos) {
     int ret = 0;
     int l = 1, r = n;
-    while (pos < r)
-    {
+    while (pos < r) {
         int mid = (l + r) >> 1;
-        if (pos <= mid)
-        {
+        if (pos <= mid) {
             r = mid;
             root = lson[root];
-        }
-        else
-        {
+        } else {
             ret += c[lson[root]];
             root = rson[root];
             l = mid + 1;
@@ -71,21 +66,45 @@ int query(int root, int pos)
     }
     return ret + c[root];
 }
-void build(int l, int n)
-{
+
+void build() {
     T[n + 1] = __build(1, n);
     map<int, int> mp;
-    for (int i = n; i >= 1; i--)
-    {
-        if (mp.find(a[i]) == mp.end())
-        {
+    for (int i = n; i >= 1; i--) {
+        if (mp.find(a[i]) == mp.end()) {
+            //没找到重复的点，直接更新
             T[i] = update(T[i + 1], i, 1);
-        }
-        else
-        {
+        } else {
+            //先在新树上去掉先前重复的点
             int tmp = update(T[i + 1], mp[a[i]], -1);
+            //在当前位置添加这个点，保持每棵树上不存在重复节点
             T[i] = update(tmp, i, 1);
         }
         mp[a[i]] = i;
     }
+}
+
+int main() {
+#ifdef ACM_LOCAL
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+
+    build();
+
+    cin >> q;
+    while (q--) {
+        int l, r;
+        cin >> l >> r;
+        cout << query(T[l], r) << endl;
+    }
+
+    return 0;
 }
