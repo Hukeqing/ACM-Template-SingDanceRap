f83dbef8043cb011a0655afb7cfc6f3556c23ef6
Mauve <keqing.hu@icloud.com>
2019-10-09 11:46:56 +0800

    修复最大流板子的bug

--- 4 图/4.7 网络流/4.7.1 ISAP邻接表栈优化.cpp
+++ 4 图/4.7 网络流/4.7.1 ISAP邻接表栈优化.cpp
@@ -1,82 +1,122 @@
-struct ISAP {
-    int Q[MAXN];
-    int S[MAXN];
-    int gap[MAXN], dep[MAXN], cur[MAXN];
+/*
+ * ISAP+bfs 初始化 + 栈优化
+ */
 
-    void BFS(int start, int end, Graph &graph) {
-        memset(dep, -1, sizeof(dep));
-        memset(gap, 0, sizeof(gap));
-        gap[0] = 1;
-        int front = 0, rear = 0;
-        dep[end] = 0;
-        Q[rear++] = end;
-        while (front != rear) {
-            int u = Q[front++];
-            for (int i = graph.head[u]; i != -1; i = graph.edge[i].next) {
-                int v = graph.edge[i].to;
-                if (dep[v] != -1)
-                    continue;
-                Q[rear++] = v;
-                dep[v] = dep[u] + 1;
-                gap[dep[v]]++;
-            }
+const int MAXN = 100010; // 点数
+const int MAXM = 400010; // 边数
+const int INF = 0x7fffffff;
+struct Edge
+{
+    int to, next, cap, flow;
+} edge[MAXM];
+int tol;
+int head[MAXN];
+int gap[MAXN], dep[MAXN], cur[MAXN];
+void init()
+{
+    tol = 0;
+    memset(head, -1, sizeof(head));
+}
+void addedge(int u, int v, int w, int rw = 0)
+{
+    edge[tol].to = v;
+    edge[tol].cap = w;
+    edge[tol].flow = 0;
+    edge[tol].next = head[u];
+    head[u] = tol++;
+    edge[tol].to = u;
+    edge[tol].cap = rw;
+    edge[tol].flow = 0;
+    edge[tol].next = head[v];
+    head[v] = tol++;
+}
+int Q[MAXN];
+void BFS(int start, int end)
+{
+    memset(dep, -1, sizeof(dep));
+    memset(gap, 0, sizeof(gap));
+    gap[0] = 1;
+    int front = 0, rear = 0;
+    dep[end] = 0;
+    Q[rear++] = end;
+    while (front != rear)
+    {
+        int u = Q[front++];
+        for (int i = head[u]; i != -1; i = edge[i].next)
+        {
+            int v = edge[i].to;
+            if (dep[v] != -1)
+                continue;
+            Q[rear++] = v;
+            dep[v] = dep[u] + 1;
+            gap[dep[v]]++;
         }
     }
-
-    int solve(int start, int end, int N, Graph &graph) {
-        BFS(start, end, graph);
-        memcpy(cur, graph.head, sizeof(graph.head));
-        int top = 0;
-        int u = start;
-        int ans = 0;
-        while (dep[start] < N) {
-            if (u == end) {
-                int Min = INT_MAX;
-                int inser = 0;
-                for (int i = 0; i < top; i++) {
-                    if (Min > graph.edge[S[i]].cap - graph.edge[S[i]].flow) {
-                        Min = graph.edge[S[i]].cap - graph.edge[S[i]].flow;
-                        inser = i;
-                    }
-                }
-                for (int i = 0; i < top; i++) {
-                    graph.edge[S[i]].flow += Min;
-                    graph.edge[S[i] ^ 1].flow -= Min;
-                }
-                ans += Min;
-                top = inser;
-                u = graph.edge[S[top] ^ 1].to;
-                continue;
-            }
-            bool flag = false;
-            int v = 0;
-            for (int i = cur[u]; i != -1; i = graph.edge[i].next) {
-                v = graph.edge[i].to;
-                if (graph.edge[i].cap - graph.edge[i].flow && dep[v] + 1 == dep[u]) {
-                    flag = true;
-                    cur[u] = i;
-                    break;
+}
+int S[MAXN];
+int sap(int start, int end, int N)
+{
+    BFS(start, end);
+    memcpy(cur, head, sizeof(head));
+    int top = 0;
+    int u = start;
+    int ans = 0;
+    while (dep[start] < N)
+    {
+        if (u == end)
+        {
+            int Min = INF;
+            int inser;
+            for (int i = 0; i < top; i++)
+            {
+                if (Min > edge[S[i]].cap - edge[S[i]].flow)
+                {
+                    Min = edge[S[i]].cap - edge[S[i]].flow;
+                    inser = i;
                 }
             }
-            if (flag) {
-                S[top++] = cur[u];
-                u = v;
-                continue;
+            for (int i = 0; i < top; i++)
+            {
+                edge[S[i]].flow += Min;
+                edge[S[i] ^ 1].flow -= Min;
             }
-            int Min = N;
-            for (int i = graph.head[u]; i != -1; i = graph.edge[i].next)
-                if (graph.edge[i].cap - graph.edge[i].flow && dep[graph.edge[i].to] < Min) {
-                    Min = dep[graph.edge[i].to];
-                    cur[u] = i;
-                }
-            gap[dep[u]]--;
-            if (!gap[dep[u]])
-                return ans;
-            dep[u] = Min + 1;
-            gap[dep[u]]++;
-            if (u != start)
-                u = graph.edge[S[--top] ^ 1].to;
+            ans += Min;
+            top = inser;
+            u = edge[S[top] ^ 1].to;
+            continue;
         }
-        return ans;
+        bool flag = false;
+        int v;
+        for (int i = cur[u]; i != -1; i = edge[i].next)
+        {
+            v = edge[i].to;
+            if (edge[i].cap - edge[i].flow && dep[v] + 1 == dep[u])
+            {
+                flag = true;
+                cur[u] = i;
+                break;
+            }
+        }
+        if (flag)
+        {
+            S[top++] = cur[u];
+            u = v;
+            continue;
+        }
+        int Min = N;
+        for (int i = head[u]; i != -1; i = edge[i].next)
+            if (edge[i].cap - edge[i].flow && dep[edge[i].to] < Min)
+            {
+                Min = dep[edge[i].to];
+                cur[u] = i;
+            }
+        gap[dep[u]]--;
+        if (!gap[dep[u]])
+            return ans;
+        dep[u] = Min + 1;
+        gap[dep[u]]++;
+        if (u != start)
+            u = edge[S[--top] ^ 1].to;
     }
-};
+    return ans;
+}
