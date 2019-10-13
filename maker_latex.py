de46d1f34c26f97315da022ed127edd0eea201b0
Mauve <keqing.hu@icloud.com>
2019-10-13 14:09:50 +0800

    更新板子

--- maker_latex.py
+++ maker_latex.py
@@ -1,4 +1,17 @@
 # python3
+"""
+> 此 python 代码将会帮你打包一个文件夹内的所有代码文件，并生成一个 Latex 文件
+
+args 额外命令参数说明：
+
+原则：出现相同/相同功能的参数时，后者参数会覆盖前者的参数，参数非法不会报错，但也不会生效。按照优先级小的先执行
+
+命令列表：
+
+1: -h           无参数，将文件和文件夹的名称第一段空格前面的所有内容不加入 Latex，可以改用 -fh, -dh 来只针对文件/文件夹生效，优先级：1
+
+2: -e           无参数，将文件和文件夹的名称扩展名不加入 Latex，可以改用 -fe, -de 来只针对文件/文件夹生效，优先级：2
+"""
 import json
 import re
 from pathlib import Path
@@ -7,6 +20,44 @@ import platform
 import functools
 
 str2utf = functools.partial(bytes, encoding='utf-8')
+
+
+class Args:
+    title_universal_args = {'-e': False, '-h': False,}
+    title_detail_args = {'-de': False, '-dh': False,
+                         '-fe': False, '-fh': False}
+
+    def __init__(self, args):
+        self.dict_args = dict()
+        self.set_args = set()
+        self.args_opt(args=args)
+
+    def args_opt(self, args):
+        argv = args.split('')
+        index = 0
+        while index < len(argv):
+            if argv[index] in Args.title_universal_args:
+                if Args.title_universal_args[argv[index]]:
+                    self.dict_args['-d' + argv[index][1:]] = argv[index + 1]
+                    self.dict_args['-f' + argv[index][1:]] = argv[index + 1]
+                    index += 1
+                else:
+                    self.set_args.add('-d' + argv[index][1:])
+                    self.set_args.add('-f' + argv[index][1:])
+            elif argv[index] in Args.title_detail_args:
+                if Args.title_detail_args[argv[index]]:
+                    self.dict_args[argv[index]] = argv[index + 1]
+                    index += 1
+                else:
+                    self.set_args.add(argv[index])
+
+    def in_set_args(self, arg):
+        return arg in self.set_args
+
+    def in_dict_args(self, arg):
+        if arg in self.dict_args:
+            return self.dict_args[arg]
+        return None
 
 
 class LatexJson:
@@ -22,7 +73,8 @@ class LatexJson:
                  code_font: str = '',
                  should_match: Optional[List[str]] = '',
                  ignore_list: Optional[List[str]] = '',
-                 save_file: Optional[str] = ''):
+                 save_file: Optional[str] = '',
+                 args: Optional[str] = ''):
         self.main_title = main_title
         self.subtitle = subtitle
         self.author = author
@@ -35,6 +87,7 @@ class LatexJson:
         self.should_match = [r'.+'] if should_match == '' else should_match if isinstance(should_match, list) else [should_match]
         self.ignore_list = [] if ignore_list == '' else ignore_list if isinstance(ignore_list, list) else [ignore_list]
         self.saver_file = 'Latex.tex' if save_file == '' else save_file
+        self.args = args
 
 
 class LatexCodeMaker:
@@ -52,6 +105,8 @@ class LatexCodeMaker:
             self.should_match: List[str] = notation['should_match']
             self.ignore_list: List[str] = notation['ignore_list']
             self.saver_file = open(notation['saver_file'], 'wb')
+
+            self.args = Args(notation['args'])
         except:
             print('样式表(JSON)出错，请新建样式表')
             exit(-1)
@@ -119,13 +174,13 @@ class LatexCodeMaker:
 
     def add_tex(self, path_object: Path, n=0):
         if path_object.is_file():
-            self.saver_file.write(str2utf(LatexCodeMaker.make_title(path_object.name, is_file=True, n=n)))
+            self.saver_file.write(str2utf(self.make_title(path_object.name, file_or_dir='f', n=n)))
             self.saver_file.write(str2utf(r'\begin{lstlisting}' + '\n'))
             self.saver_file.write(LatexCodeMaker.read_file(path_object, self.decode_mode))
             self.saver_file.write(str2utf(r'\end{lstlisting}' + '\n'))
             return False
         elif path_object.is_dir():
-            self.saver_file.write(str2utf(LatexCodeMaker.make_title(path_object.name, is_file=False, n=n)))
+            self.saver_file.write(str2utf(self.make_title(path_object.name, file_or_dir='d', n=n)))
             return True
 
     def judge(self, name: str):
@@ -145,21 +200,22 @@ class LatexCodeMaker:
                 if self.add_tex(item, n):
                     self.direction_ergodic(item, n + 1)
 
-    @staticmethod
-    def make_title(title, is_file, n=0):
-        title = title.split(' ', 1)[-1].replace('%', '\%').replace('&', '\&').replace('~', '\~').replace('_', '\_').replace('^', '\^')
-        if is_file:
-            title = title[::-1].split('.', 1)[1][::-1]
+    def make_title(self, title, file_or_dir, n=0):
+        title: str = title.replace('%', r'\%').replace('&', r'\&').replace('~', r'\~').replace('_', r'\_').replace('^', r'\^')
+        if self.args.in_set_args('-' + file_or_dir + 'h'):
+            title: str = title.split(maxsplit=1)[1]
+        if self.args.in_set_args('-' + file_or_dir + 'e'):
+            title: str = title[::-1].split(maxsplit=1)[1][::-1]
         if n == 0:
-            return '\section{' + title + '}\n'
+            return r'\section{' + title + '}\n'
         elif n == 1:
-            return '\subsection{' + title + '}\n'
+            return r'\subsection{' + title + '}\n'
         elif n == 2:
-            return '\subsubsection{' + title + '}\n'
+            return r'\subsubsection{' + title + '}\n'
         elif n == 3:
-            return '\paragraph{' + title + '}\n'
+            return r'\paragraph{' + title + '}\n'
         else:
-            return '\subparagraph{' + title + '}\n'
+            return r'\subparagraph{' + title + '}\n'
 
     @staticmethod
     def read_file(path_object: Path, decode_mode):
@@ -194,13 +250,14 @@ class LatexSetting:
                                author=input('请输入作者: '),
                                padding=tuple([int(t) for t in input('请输入纸面边距(顺序: 左右上下，单位: mm，单个空格隔开): ').split(' ')]),
                                direction_path=input('请输入目录文件夹位置(可以使用相对位置，留空则为当前目录): '),
-                               decode_mode=input('请输入你的文件(指输入文件，输出的tex文件默认为utf-8)编码格式("utf-8" or "gb2312"，默认为utf-8): '),
+                               decode_mode=input('请输入你的文件(非输出文件，输出的tex文件默认为utf-8)编码格式("utf-8" or "gb2312"，默认为utf-8): '),
                                logo_path=input('请输入封面logo文件位置(可以使用相对位置，留空则无): '),
                                main_font=input('请输入文档目录等字体(留空为"Microsoft YaHei"): '),
                                code_font=input('请输入代码字体(留空为"Consolas"): '),
                                should_match=input('请输入需要处理的文件(文件夹)的文件名通用样式(留空则为全部文件，正则表达式全匹配模式，多种模式则用单个空格隔开): ').split(' '),
                                ignore_list=input('请输入免处理的文件(文件夹)的文件名通用样式(留空则为全部文件，正则表达式全匹配模式，多种模式则用单个空格隔开): ').split(' '),
-                               save_file=input('请输入最终保存的文件名(默认为Latex.tex): '))
+                               save_file=input('请输入最终保存的文件名(默认为Latex.tex): '),
+                               args=input('请输入额外参数(具体请参考python代码最上方的说明): '))
         self.data = json_class.__dict__
         op = input('完成设置(样式表/JSON文件: setting.json)！是否保存的设置？[Y/N]: ')
         if op not in ['n', 'N', 'no', 'No', 'NO']:
