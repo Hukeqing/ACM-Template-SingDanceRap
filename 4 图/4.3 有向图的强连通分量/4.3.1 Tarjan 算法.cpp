a548bc9331ef13a9b8dd6d24201a31e503761691
Mauve <keqing.hu@icloud.com>
2019-10-13 16:59:20 +0800

    更新tarjan算法

--- 4 图/4.3 有向图的强连通分量/4.3.1 Tarjan 算法.cpp
+++ 4 图/4.3 有向图的强连通分量/4.3.1 Tarjan 算法.cpp
@@ -7,10 +7,9 @@
  * 点编号 1 - n
  */
 
-const int MAXN = 10010;  // 点数
-const int MAXM = 100010; // 边数
-struct Edge
-{
+const int MAXN = 1100;  // 点数
+const int MAXM = 1000100; // 边数
+struct Edge {
     int to, next;
 } edge[MAXM]; // 只有这里写的是 MAXM
 
@@ -22,36 +21,29 @@ bool Instack[MAXN];
 int num[MAXN]; // 各个强连通分量包含点的个数，数组编号 1 ～ scc
 // num 数组不一定需要，结合实际情况
 
-void addedge(int u, int v)
-{
+void addedge(int u, int v) {
     edge[tot].to = v;
     edge[tot].next = head[u];
     head[u] = tot++;
 }
 
-void Tarjan(int u)
-{
+void Tarjan(int u) {
     int v;
     Low[u] = DFN[u] = ++Index;
     Stack[top++] = u;
     Instack[u] = true;
-    for (int i = head[u]; i != -1; i = edge[i].next)
-    {
+    for (int i = head[u]; i != -1; i = edge[i].next) {
         v = edge[i].to;
-        if (!DFN[v])
-        {
+        if (!DFN[v]) {
             Tarjan(v);
             if (Low[u] > Low[v])
                 Low[u] = Low[v];
-        }
-        else if (Instack[v] && Low[u] > DFN[v])
+        } else if (Instack[v] && Low[u] > DFN[v])
             Low[u] = DFN[v];
     }
-    if (Low[u] == DFN[u])
-    {
+    if (Low[u] == DFN[u]) {
         scc++;
-        do
-        {
+        do {
             v = Stack[--top];
             Instack[v] = false;
             Belong[v] = scc;
@@ -59,8 +51,8 @@ void Tarjan(int u)
         } while (v != u);
     }
 }
-void solve(int N)
-{
+
+void solve(int N) {
     memset(DFN, 0, sizeof(DFN));
     memset(Instack, false, sizeof(Instack));
     memset(num, 0, sizeof(num));
@@ -69,8 +61,8 @@ void solve(int N)
         if (!DFN[i])
             Tarjan(i);
 }
-void init()
-{
+
+void init() {
     tot = 0;
     memset(head, -1, sizeof(head));
 }
