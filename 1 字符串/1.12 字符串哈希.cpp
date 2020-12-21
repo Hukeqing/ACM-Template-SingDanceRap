52edfd8a0473c68e892d2c7e4f84f6325ce6224e
EscapeReality846089495 <846089495@qq.com>
2020-12-21 14:35:21 +0800

    Update 1.12 字符串哈希.cpp

--- 1 字符串/1.12 字符串哈希.cpp
+++ 1 字符串/1.12 字符串哈希.cpp
@@ -6,9 +6,9 @@ using namespace std;
 typedef unsigned long long ull;
 
 ull ha[NUM], fac[NUM];
-ull seed = 201326611;// 201326611 233 50331653 212370440130137957ll
+ull seed = 201326611;// 201326611 233 50331653 212370440130137957ll 19260817 19660813
 char str[NUM];
-
+// 无取模版本
 /**
  * 获取子串[l, r]（均为闭区间）的哈希值
  */
@@ -19,10 +19,6 @@ ull getStr(int l, int r) {
 }
 
 int main() {
-#ifdef ACM_LOCAL
-    freopen("in.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
-#endif
     fac[0] = 1;
     for (int i = 1; i < NUM; i++)
         fac[i] = fac[i - 1] * seed;
@@ -31,5 +27,25 @@ int main() {
     ha[0] = str[0];
     for (int i = 1; i < size; i++)
         ha[i] = ha[i - 1] * seed + str[i];
+    return 0;
+}
+// 取模版本
+/**
+ * 获取子串[l, r]（均为闭区间）的哈希值
+ */
+ull getStr(int l, int r) {
+    if (l == 0) return ha[r];
+    return (ha[r] - ha[l - 1] * fac[r - l + 1] % mod + mod) % mod;
+}
+
+int main() {
+    fac[0] = 1;
+    for (int i = 1; i < NUM; i++) fac[i] = fac[i - 1] * seed % mod;
+    while (cin >> M >> L >> s) {
+        int size = s.length();
+        ha[0] = (ull) s[0];
+        for (int i = 1; i < size; i++)
+            ha[i] = (ha[i - 1] * seed % mod + s[i]) % mod;
+    }
     return 0;
 }
